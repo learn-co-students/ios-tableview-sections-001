fe4af49024ea677bc208d1718618b1d70a7ebc1b
joshmotley <joshkmotley@gmail.com>
2016-01-23 22:30:34 -0500

    implement section (with headers) in a table view

---
+++ FISStudent.h
@@ -0,0 +1,16 @@
+//
+//  FISStudent.h
+//  
+//
+//  Created by Joshua Motley on 1/23/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISStudent : NSObject
+
+@property (strong, nonatomic) NSArray *favoriteThings;
+@property (strong, nonatomic) NSString *name;
+
+@end
